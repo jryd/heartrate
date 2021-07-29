fe505edc2829a7d24b15371c6b623b7f0857cd50
James <james.b@iseekplant.com.au>
2021-07-29 21:39:01 +1000

    Connect to selected heartrate monitor
    Closes #2

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import React  from 'react'
 
 const App = () => {
   const pair = async () => {
-    const device = await navigator.bluetooth.requestDevice({
-      filters: [{ services: ['heart_rate'] }],
-      acceptAllDevices: false,
-    });
+    try {
+      const device = await navigator.bluetooth.requestDevice({
+        filters: [{services: ['heart_rate']}],
+        acceptAllDevices: false,
+      });
+
+      const server = await device.gatt.connect();
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return <button onClick={pair}>Pair to heartrate monitor</button>
