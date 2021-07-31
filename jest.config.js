74293f093ae23fd6a224e0dc09d36eaf23243d9c
James <james.b@iseekplant.com.au>
2021-07-31 13:06:41 +1000

    Add Jest testing

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  roots: [
+    '<rootDir>/src'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+  ],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$'
+  ],
+  resetMocks: true
+};
