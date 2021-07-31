995c9f038798afc0f03abbcb8f58f47240ec0cff
James <james.b@iseekplant.com.au>
2021-07-31 12:58:40 +1000

    Remove test extension

--- jest.config.js
+++ jest.config.js
@@ -7,10 +7,6 @@ module.exports = {
     'src/**/*.{js,jsx,ts,tsx}',
     '!src/**/*.d.ts'
   ],
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'
-  ],
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
